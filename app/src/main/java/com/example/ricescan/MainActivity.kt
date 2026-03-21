2d1d6841b236b9ab89e53a00f84cb4a755c90648
Utkrista <utkristashrestha984@gmail.com>
2026-03-21 22:00:27 +0545

    first commit

---
+++ app/src/main/java/com/example/ricescan/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ricescan
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ricescan.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/ricescan/result/ResultFragment.kt
@@ -0,0 +1,17 @@
+package com.example.ricescan.result
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.ricescan.R
+
+class ResultFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_result, container, false)
+    }
+}
